fa70d069a784aed526713baa539ec0b47dfa2011
Gabriela <gmartinezmenegatty@danielcastelao.org>
2026-03-24 14:15:14 +0100

    Añadido comentarios

--- Nave.py
+++ Nave.py
@@ -1,17 +1,19 @@
+#Clase que almacena la nave
+
 class Nave:
 
     def __init__(self, nombre, tipo, vida):
-        self.nombre = nombre
-        self.tipo = tipo
-        self.vida = vida
+        self.nombre = nombre #Inicializamos el nombre de la nave
+        self.tipo = tipo #Inicializamos el tipo de la nave
+        self.vida = vida #Inicializamos la Vida de la nave
 
     def recibir_disparo(self):
-        self.vida -= 1
+        self.vida -= 1 #La vida se ira restando
 
-        if self.vida > 0:
-            return 1
+        if self.vida > 0: #Si la vida es mayor que 0
+            return 1 # Se toca una nave
         else:
-            return 2
+            return 2 # Si es menor que 0 estara Hundida
 
     def __str__(self):
-        return f"{self.nombre} {self.tipo} {self.vida}"
+        return f"{self.nombre} {self.tipo} {self.vida}" #Mostramos el resultado
